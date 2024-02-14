e745946ee3ce0c9e7e1ad1e114287838da3188b0
hridoythebest <hridoythebest@gmail.com>
2024-02-14 18:55:58 +0600

    headline section card responsive done

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeadlineCards = () => {
   return (
-    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
       {/* Card */}
       <div className='rounded-xl relative'>
         {/* Overlay */}
@@ -13,7 +13,7 @@ const HeadlineCards = () => {
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-          src='https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          src='https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
           alt='/'
         />
       </div>
@@ -23,11 +23,11 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
           <p className='px-2'>Added Daily</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4 ju'>Order Now</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-          src='https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          src='https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
           alt='/'
         />
       </div>
