21812e01c454e2de1bb5913dc8f70fd3e52cd4aa
Md Hridoy Hossain <58304677+hridoythebest@users.noreply.github.com>
2024-02-23 09:11:34 +0600

    Update data.js
    New data added on api

--- src/data/data.js
+++ src/data/data.js
@@ -178,5 +178,29 @@ export const data = [
       image:
         'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
     },
+      {
+      id: 5,
+      name: 'Latest Deals',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+    },
+    {
+      id: 6,
+      name: 'Restaurant Rewards',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+    },
+    {
+      id: 7,
+      name: 'Best Overall',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+    },
+    {
+      id: 8,
+      name: 'Shipped Free',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+    },
   ];
   
